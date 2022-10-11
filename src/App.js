12b17f9439d955e2b83c08fcdb4bce3d3f386e19
Zin Min Htet <zinminn34119@gmail.com>
2022-10-11 15:06:21 +0630

    test-one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import User from "./components/user/user";
 
 function App() {
+
+  let users = [
+    {
+      id:1,
+      image:"https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png",
+      phone: "09797134",
+      cell: "09100100",
+      name: "Mg Mg"
+    },
+    {
+      id:2,
+      image:"https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png",
+      phone: "09159779",
+      cell: "09200200",
+      name: "Su Su"
+    },
+    {
+      id:3,
+      image:"https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png",
+      phone: "09816735",
+      cell: "09300300",
+      name: "Aung Gyi"
+    },
+  ]
+
+  let [count, setCount] = useState(5);
+  let [name, setName] = useState("Aung Aung")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container mt-5">
+        {/* <User image={users[0].image} phone= {users[0].phone} cell= {users[0].cell} name= {users[0].name} />
+        <User image={users[1].image} phone= {users[1].phone} cell= {users[1].cell} name= {users[1].name} /> */}
+
+        <h4>{count}</h4>
+        <h4>{name}</h4>
+
+        <button className="btn btn-primary btn-sm my-4" onClick={()=>{
+          setCount(++count);
+          setName(name === "Aung Aung" ? "Su Su" : "Aung Aung")
+          console.log("Count is",count);
+        }}>Count</button>
+
+        {users.map(u => <User key={u.id} data={u} />)}
+        
+      </div>
   );
 }
 
---
+++ src/components/user/user.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function User (props) {
+    return (
+        <div className="card mb-3">
+        <div className="row">
+        <div className="col-3">
+        <img src={props.data.image} width="50px" height="50px"></img>
+        </div>
+        <div className="col-4"> 
+        <strong>Ph: {props.data.phone}</strong>
+        <br></br>
+        <strong>Cell: {props.data.cell}</strong>
+        </div >
+        <div className="col-3 mt-2">
+            <strong>
+                {props.data.name}
+            </strong>
+        </div>
+        <div className="col-2">
+        <button className="btn btn-danger btn-sm mt-2">
+        <i className="fa-solid fa-trash"></i>
+        </button>
+        </div>
+        
+        </div>
+        </div>
+        );
+    }
+    
+    export default User;
