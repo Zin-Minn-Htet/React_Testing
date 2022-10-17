e60e13e0a1d12013ac9f8ab516a4ada29ebc4f3f
Zin Min Htet <zinminn34119@gmail.com>
2022-10-17 11:45:00 +0630

    test-style-component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AddPost from "./components/AddPost";
-import Post from "./components/Post";
-import PostDetail from "./components/PostDetail";
-import EditPost from "./components/EditPost";
+import React from "react";
+import CompoA from "./components/CompoA";
+import CompoB from "./components/CompoB";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./style/GlobalStyle";
+
+const theme = {
+  colors: {
+    primary: "blue",
+    secondary: "hotpink",
+    third: "gray"
+  },
+  size: {
+    h1Size: "50px",
+    h2Size: "30px"
+  }
+}
+
 
 function App() {
-  const END_POINT = "http://localhost:9000/posts";
-
-  let [posts, setPosts] = useState([]);
-
-  const addPosts = async(post) => {
-    await fetch(END_POINT,{
-      method: "POST",
-      body: JSON.stringify({
-        title: post.title,
-        desc: post.desc
-      }),
-      headers:{
-        "content-type":"application/json"
-      }
-    });
-    setPosts([post, ...posts])
-  }
-
-  useEffect(() => {
-    async function fetchData() {
-      let posts = await (await fetch(END_POINT)).json();
-      setPosts(posts);
-    }
-    fetchData();
-  }, []);
-
-  const postDeleteHandler = async(id) => {
-    await fetch(END_POINT+"/"+id,{
-      method: "DELETE"
-    })
-    setPosts(posts.filter(post => post.id != id))
-  }
-
-  const updatePostHandler = async(updatePost) => {
-    await fetch(END_POINT+"/"+updatePost.id,{
-      method: "PATCH",
-      body: JSON.stringify(updatePost),
-      headers: {
-        "content-type" : "application/json"
-      }
-
-    })
-    setPosts(posts.map(po => po.id === updatePost.id ? updatePost : po) )
-  }
-
   return (
-    <div className="container">
-      <h2 className="text-center text-info">Posts</h2>
-
-      <Router>
-        <Routes>
-          <Route path="/" element={<Post posts={posts} removePost={postDeleteHandler} />} />
-          <Route path="/add" element={<AddPost addPost={addPosts} />} />
-          <Route path="/post/:id" element={ <PostDetail />} />
-          <Route path="/post/edit/:id" element={ <EditPost updatePost={updatePostHandler} /> } />
-        </Routes>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div>
+        <CompoA />
+        <CompoB />
+      </div>
+    </ThemeProvider>
   )
 }
 
---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,13 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: black
+    }
+    a {
+        text-decoration : none;
+        color : white;
+    }
+
+`
+export default GlobalStyle;
