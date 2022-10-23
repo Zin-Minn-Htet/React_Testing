57cfc4698c86e638d97c9b0ba7f1cdd9f502f8bd
Zin Min Htet <zinminn34119@gmail.com>
2022-10-23 09:57:12 +0630

    Redux Login

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-import { useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import Acompo from "./components/Acompo";
-import { actionCreators } from './store/actionCreators';
+import React from "react";
+import {useSelector,useDispatch} from 'react-redux';
+import { bindActionCreators } from "redux";
+import { actionCreator } from "./stores/actionCreator";
+
+
 
 function App() {
-  const account = useSelector(state => state.account);
-  const user = useSelector(state => state.user)
-  const dispatch = useDispatch();
-  const { add, remove, drop, inset } = bindActionCreators(actionCreators, dispatch);
-  const nameRef = useRef();
-  const phoneRef = useRef();
-  const addUser = (e) => {
-    e.preventDefault();
-    let user = {
-      name: nameRef.current.value,
-      phone: phoneRef.current.value
-    }
-    inset(user)
-    nameRef.current.value = "";
-    phoneRef.current.value = ""
-  }
 
+  const logined = useSelector(state => state.login);
+  const dispatch = useDispatch();
+  const {login,logout} = bindActionCreators(actionCreator,dispatch);
 
   return (
     <div>
-      <h1>{account}</h1>
-      <button onClick={() => add(50)}>Add</button>
-      <button onClick={() => remove(50)}>Remove</button>
-      {
-        user.map(u => <p onClick={() => drop(user)} > {u.name}  {u.phone} </p>)
-      }
-      <form onSubmit={addUser}>
-        <input type="phone" ref={phoneRef} />
-        <br></br>
-        <br></br>
-        <input type="name" ref={nameRef} />
-        <button type="submit">addUser</button>
-      </form>
-      <Acompo />
+      <h1> {logined ? "Member" : "Guest"} </h1>
+      <button onClick={() => login(true)}>login</button>
+      <button onClick={() => logout(false)}>logout</button>
     </div>
   )
 }
---
+++ src/stores/actions/loginAction.js
@@ -0,0 +1,17 @@
+export const login = (con) => {
+    return (dispatch) => {
+        dispatch({
+            type: "login",
+            payload: con
+        })
+    }
+}
+
+export const logout = (con) => {
+    return (dispatch) => {
+        dispatch({
+            type: "logout",
+            payload: con
+        })
+    }
+}
